5307b11342ed153701c6f9c29f01cf2ce151ffef
nguyenthinh28902 <nndthinh28902@gmail.com>
2026-03-24 14:27:36 +0700

    fix build entity

---
+++ Ecom.OrderService.Infrastructure/DbContexts/EcomOrderDbContext.Manual.cs
@@ -0,0 +1,20 @@
+﻿using Ecom.OrderService.Core.Models.Auth;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecom.OrderService.Infrastructure.DbContexts
+{
+    public partial class EcomOrderDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Đưa logic dùng biến hằng của bạn vào đây
+                optionsBuilder.UseSqlServer(ConnectionStrings.EcomOrderConnectionString);
+            }
+        }
+    }
+}
